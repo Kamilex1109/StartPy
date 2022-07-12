205bc8c6f37c7cc24ab13b0865c9a3b32bf87307
Kamilex1109 <xelimak1109@wp.pl>
2022-07-12 12:37:10 +0200

    Testing

--- Kurs nr 1 - Kanał o wszystkim/Brudnopis.py
+++ Kurs nr 1 - Kanał o wszystkim/Brudnopis.py
@@ -8,6 +8,12 @@ ZmiennaY = input("w którym roku się urodziłeś? ")
 ZmiennaZ = 2022-int(ZmiennaY)
 print("Masz "+str(ZmiennaZ) + " lat")
 """
-
+"""
+Imie = input("Podaj hasło: ")
+if int(Imie) == 1234:
+    print("Hasło prawidłowe!")
+else:
+    print("Hasło nieprawidłowe!")
+"""
 
 
---
+++ Kurs nr 1 - Kanał o wszystkim/Lekcja 3.py
@@ -0,0 +1,27 @@
+
+x = True
+y = False
+"""
+print(True)
+print(False)
+
+#Operatory porównania
+print(5 == 1)       #równośći - albo prawdziwe albo fałszywe
+print(5 != 1)       #różności -
+print(5 < 1)        #nierówności
+print(5 > 1)        #nierówności
+print(5 <= 1)       #mniejszy bądź równy
+print(5 >= 1)       #większy bądź równy
+"""
+A = 7
+B = 6
+#Instrukcje warunkowe
+if A > B:
+    print("Większe")
+elif A < B:
+    print("Mniejsze")
+else:
+    print("Równy")
+
+
+
